348ccd9f3799f92723df79e2d5e567e97410b034
terenceza <158005283+terenceza@users.noreply.github.com>
2025-05-07 16:09:13 -0600

    Update LogRegressionTrainMainHost.c

--- LogRegressionTrainMainHost.c
+++ LogRegressionTrainMainHost.c
@@ -11,7 +11,7 @@
 /* Xilinx includes. */
 #include "xil_printf.h"
 #include "xparameters.h"
-#include "LogRegression.h"
+#include "LogRegressionHost.h"
 
 #define TIMER_ID	1
 #define DELAY_10_SECONDS	10000UL
